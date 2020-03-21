2551b30abff8f63f0a5ed76398109c737739571f
Apr1c07 <40536719+Apr1c07@users.noreply.github.com>
2020-03-21 20:50:32 +0900

    Update URLConversion.js

--- js/URLConversion.js
+++ js/URLConversion.js
@@ -1,10 +1,11 @@
 var InputURL;
 
 function URLConversion() {
+   alert('JavaScriptのアラート');
   InputURL = Number(document.getElementById('inputURL').value);
-  var a = InputURL.replace(/https:/ / www.dropbox.com / g, "https://dl.dropboxusercontent.com");
+  var a = InputURL.replace("https://www.dropbox.com", "https://dl.dropboxusercontent.com");
 
-  var b = a.replace(/?dl=0/g, "");
+  var b = a.replace("?dl=0", "");
 
   document.getElementById("inputURL").value = b;
 }
