93b28989b238fc543482a747f1bb5ee9d8327b77
Apr1c07 <40536719+Apr1c07@users.noreply.github.com>
2020-03-22 00:20:37 +0900

    Update URLConversion.js

--- js/URLConversion.js
+++ js/URLConversion.js
@@ -1,17 +1,13 @@
-var InputURL;
-
 function URLConversion() {
-   alert('JavaScriptのアラート');
-  InputURL = document.getElementById('inputURL').value;
+  var InputURL = document.getElementById('inputURL').value;
   var a = InputURL.replace("https://www.dropbox.com", "https://dl.dropboxusercontent.com");
-
   var b = a.replace("?dl=0", "");
-
   document.getElementById("inputURL").value = b;
-   alert(b);
-// 文字をすべて選択
-b.select();
-// コピー
-//document.execCommand("copy");
-   alert('クリップボードにコピーしました。');
+}
+
+function URLCopy(){
+  var InputURL = document.getElementById('inputURL').value;
+   InputURL.select();
+   document.execCommand("Copy");
+    alert('クリップボードにコピーしました。');
 }
