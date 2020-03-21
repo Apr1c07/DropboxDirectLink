c4e93230e60185615e4b5df7f87878d0c8ab62be
Apr1c07 <40536719+Apr1c07@users.noreply.github.com>
2020-03-21 20:39:41 +0900

    Update URLConversion.js

--- js/URLConversion.js
+++ js/URLConversion.js
@@ -1,1 +1,10 @@
-aaa
+var InputURL;
+
+function URLConversion() {
+  InputURL = Number(document.getElementById('inputURL').value);
+  var a = InputURL.replace(/https:/ / www.dropbox.com / g, "https://dl.dropboxusercontent.com");
+
+  var b = a.replace(/?dl=0/g, "");
+
+  document.getElementById("inputURL").value = b;
+}
